7b54c732e82c756443941d71bed09f067f0a0088
Samedy1 <samedyphin1@gmail.com>
2024-02-14 17:54:37 +0700

    created the class for addk interpolation

--- preprocess.py
+++ preprocess.py
@@ -1,42 +1,36 @@
-# %% [markdown]
-# ## 1. Data Preparation
-
 # %%
-# read file
-# import os
-
-# file_names = os.listdir('corpus')
-# file_content = ''
-
-# for file_name in file_names:
-#     file_path = os.path.join('corpus', file_name)
-
-#     with open(file_path, 'r') as infile:
-#         file_content = file_content + infile.read().replace('\n', '')
-
-# %%
-with open('corpus/test.txt', 'r') as infile:
-    file_content = infile.read().replace('\n', '')
-
-# %%
-from nltk import sent_tokenize
-sentences = sent_tokenize(file_content)
-
-# %%
-# splitting
-training_size = int(len(sentences) * 0.7)
-validation_size = int(len(sentences) * 0.1)
-
-training_data = sentences[:training_size]
-validation_data = sentences[training_size:training_size + validation_size]
-test_data = sentences[training_size + validation_size:]
-
-# %%
+from nltk import sent_tokenize, word_tokenize, ngrams
 import re
-from nltk import word_tokenize, ngrams
 import string
+from nltk.probability import FreqDist
 
-emoji_pattern = re.compile("["
+# %%
+class TextPreprocessor:
+    def __init__(self) -> None:
+        with open('corpus/test.txt', 'r') as infile:
+            file_content = infile.read().replace('\n', '')
+            
+        # split the file content into sentences
+        self.sentences = sent_tokenize(file_content)
+        
+        # ---------- Train, Validation, Test ----------
+        # size of training data
+        self.training_size = int(len(self.sentences) * 0.7)
+        self.training_data = self.sentences[:self.training_size]
+
+        # size of validation data
+        self.validation_size = int(len(self.sentences) * 0.1)
+        self.validation_data = self.sentences[self.training_size:self.training_size + self.validation_size]
+
+        self.test_data = self.sentences[self.training_size + self.validation_size:]
+        
+        # ---------- Call necessary methods ----------
+        # self.find_unknown_word(self.tokenize_words(self.test_data))
+        self.create_freq_n_gram(self.training_data)
+        pass
+
+    def tokenize_words(self, sentences):
+        emoji_pattern = re.compile("["
                                u"\U0001F600-\U0001F64F"  # emoticons
                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
@@ -44,172 +38,93 @@ emoji_pattern = re.compile("["
                                u"\U00002702-\U000027B0"
                                u"\U000024C2-\U0001F251"
                                "]+", flags=re.UNICODE)
-
-number_backet_pattern = re.compile(r"\d")
-                                
-def tokenize_sentences(sentences):
-    # convert to lower case
-    low_sentences = [sentence.lower() for sentence in sentences]
-
-    # remove number bracket
-    cleaned_sentences = [re.sub(number_backet_pattern, "", sentence) for sentence in low_sentences]
-
-
-    # remove emoji
-    no_emoji_sentences = [re.sub(emoji_pattern, "", sentence) for sentence in cleaned_sentences]
-
-    # split each sentence into tokens
-    tokens_2d = [word_tokenize(sentence) for sentence in no_emoji_sentences]
-
-    # remove punctuation
-    translator = str.maketrans('', '', string.punctuation)
-    no_punc_tokens = []
-    for tokens_1d in tokens_2d:
-        no_punc_tokens.append([token.translate(translator) for token in tokens_1d])
-
-    # # remove empty string
-    no_empty_tokens = []
-    for token_1d in no_punc_tokens:
-        no_empty_tokens.append([token for token in token_1d if token != ''])
-
-    # generate n-grams
-    uni_grams = []
-    bi_grams = []
-    tri_grams = []
-    four_grams = []
-
-    for token_1d in no_empty_tokens:
-        uni_grams.append(list(ngrams(token_1d, n=1)))
-        bi_grams.append(list(ngrams(token_1d, n=2)))
-        tri_grams.append(list(ngrams(token_1d, n=3)))
-        four_grams.append(list(ngrams(token_1d, n=4)))
-
-    # return a dictionary containing all lists of n-grams
-    return {
-        'sentences': no_empty_tokens,
-        'uni_grams': uni_grams,
-        'bi_grams': bi_grams,
-        'tri_grams': tri_grams,
-        'four_grams': four_grams,
-    }
-
-# %%
-# handle unknown word
-
-# %%
-tokenized_sent = tokenize_sentences(training_data)
-
-# %%
-# if (__name__ == '__main__'):
-#     for l in tokenized_sent['four_grams']:
-#         print(l)
-
-# %%
-# from nltk.probability import FreqDist
-
-
-# freq_uni = FreqDist()
-# freq_bi = FreqDist()
-# freq_tri = FreqDist()
-# freq_four = FreqDist()
-
-# # count uni-grams
-# for l in tokenized_sent['uni_grams']:
-#     for uni_gram in l:
-#         freq_uni[uni_gram] = freq_uni[uni_gram] + 1
-
-# # count bi-grams
-# for l in tokenized_sent['bi_grams']:
-#     for bi_gram in l:
-#         freq_bi[bi_gram] = freq_bi[bi_gram] + 1
+        number_bracket_pattern = re.compile(r"\d")
         
-# # count bi-grams
-# for l in tokenized_sent['tri_grams']:
-#     for tri_gram in l:
-#         freq_tri[tri_gram] = freq_tri[tri_gram] + 1
+        # convert to lower case
+        low_sentences = [sentence.lower() for sentence in sentences]
 
-# # count bi-grams
-# for l in tokenized_sent['four_grams']:
-#     for four_gram in l:
-#         freq_four[four_gram] = freq_four[four_gram] + 1
+        # remove number bracket
+        cleaned_sentences = [re.sub(number_bracket_pattern, "", sentence) for sentence in low_sentences]
 
+        # remove emoji
+        no_emoji_sentences = [re.sub(emoji_pattern, "", sentence) for sentence in cleaned_sentences]
 
-# %%
-# if (__name__ == '__main__'):
-#     for word in freq_four:
-#         print(f'{word}: {freq_four[word]}')
+        # split each sentence into tokens: [['token', ''token'], ['token', 'token'], ...]
+        tokens_2d = [word_tokenize(sentence) for sentence in no_emoji_sentences]
 
-# %%
-def tokenize_words(data):
-    words = []
-    sentences = tokenize_sentences(data)['sentences']
-    for sentence in sentences:
-        words.append('<s>')
-        words.extend(sentence)
-        words.append('</s>')
+        # remove punctuation
+        translator = str.maketrans('', '', string.punctuation)
+        no_punc_tokens = []
+        for tokens_1d in tokens_2d:
+            no_punc_tokens.append([token.translate(translator) for token in tokens_1d])
 
-    # create n-grams
-    uni_grams = []
-    bi_grams = []
-    tri_grams = []
-    four_grams = []
+        # # remove empty string
+        no_empty_tokens = []
+        for token_1d in no_punc_tokens:
+            no_empty_tokens.append([token for token in token_1d if token != ''])
+            
+        # convert into token list: ['token', 'token']
+        tokens = []
+        for sentence in no_empty_tokens:
+            # add start and end tokens to identify sentence boundary
+            tokens.append('<s>')
+            
+            # add words in each sentence into tokens
+            tokens.extend(sentence)
+            tokens.append('</s>')
 
-    uni_grams.extend(list(ngrams(words, n=1)))
-    bi_grams.extend(list(ngrams(words, n=2)))
-    tri_grams.extend(list(ngrams(words, n=3)))
-    four_grams.extend(list(ngrams(words, n=4)))
+        # create n-grams
+        uni_grams = []
+        bi_grams = []
+        tri_grams = []
+        four_grams = []
 
-    return {
-        'sentences': words,
-        'uni_grams': uni_grams,
-        'bi_grams': bi_grams,
-        'tri_grams': tri_grams,
-        'four_grams': four_grams,
-    } 
-    
+        uni_grams.extend(list(ngrams(tokens, n=1)))
+        bi_grams.extend(list(ngrams(tokens, n=2)))
+        tri_grams.extend(list(ngrams(tokens, n=3)))
+        four_grams.extend(list(ngrams(tokens, n=4)))
 
-# %%
-# if (__name__ == '__main__'):
-#     for l in tokenize_words(training_data)['four_grams']:
-#         print(l)
-
-# %%
-# tokenize_words()['uni_grams']
-    
-
-# %%
-from nltk.probability import FreqDist
-
-
-freq_uni = FreqDist()
-freq_bi = FreqDist()
-freq_tri = FreqDist()
-freq_four = FreqDist()
-
-tokenized_words = tokenize_words(training_data);
-
-# count uni-grams
-for uni_gram in tokenized_words['uni_grams']:
-    freq_uni[uni_gram] = freq_uni[uni_gram] + 1
-
-# count bi-grams
-for bi_gram in tokenized_words['bi_grams']:
-    freq_bi[bi_gram] = freq_bi[bi_gram] + 1
+        return {
+            'sentences': tokens,
+            'uni_grams': uni_grams,
+            'bi_grams': bi_grams,
+            'tri_grams': tri_grams,
+            'four_grams': four_grams,
+        }
         
-# count bi-grams
-for tri_gram in tokenized_words['tri_grams']:
-    freq_tri[tri_gram] = freq_tri[tri_gram] + 1
+    def create_freq_n_gram(self, training_data):
+        # create a frequency distribution for each n-gram
+        self.freq_uni = FreqDist()
+        self.freq_bi = FreqDist()
+        self.freq_tri = FreqDist()
+        self.freq_four = FreqDist()
 
-# count bi-grams
-for four_gram in tokenized_words['four_grams']:
-    freq_four[four_gram] = freq_four[four_gram] + 1
+        tokenized_words = self.tokenize_words(training_data);
+        
+        # count uni-grams
+        for uni_gram in tokenized_words['uni_grams']:
+            self.freq_uni[uni_gram] = self.freq_uni[uni_gram] + 1
 
+        # count bi-grams
+        for bi_gram in tokenized_words['bi_grams']:
+            self.freq_bi[bi_gram] = self.freq_bi[bi_gram] + 1
+                
+        # count bi-grams
+        for tri_gram in tokenized_words['tri_grams']:
+            self.freq_tri[tri_gram] = self.freq_tri[tri_gram] + 1
 
-# %%
-# if (__name__ == '__main__'):
-    # print(tokenize_sentences(sentences)['sentences'])
-    # print(freq_uni.get(('is',), 0))
-    # for word in freq_four:
-    #     print(f'{word}: {freq_four[word]}')
-
-
+        # count bi-grams
+        for four_gram in tokenized_words['four_grams']:
+            self.freq_four[four_gram] = self.freq_four[four_gram] + 1
+            
+    # handle unknown word
+    # def find_unknown_word(self, training:list, testing:list):
+    #     training = self.tokenize_words(training)
+    #     for word in training:
+    #         if word not in testing:
+    #             training[training.index(word)] = '<UNK>'
+                
+    #     for word in testing:
+    #         if word not in training:
+    #             testing[testing.index(word)] = '<UNK>'
+    #         i = i + 1
